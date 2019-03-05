5ca98406a4361ac0fec4e915bcff9ed761b37638
selviseptianiii <septianiselvi6@gmail.com>
2019-03-04 14:43:29 -1200

    Eps 4

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->group(function(){
-	Route::get('/', function(){
-		return view('admin.pages.dashboard');
+Route::group(['middleware'=>['auth']], function(){
+	Route::prefix('admin')->group(function(){
+		Route::get('/', function(){
+		 return view('admin.pages.dashboard');
 	})->name('admin.home');
+  });
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
